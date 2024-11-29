49ae75984518bfe8e91aba60177dc9e509a1ce24
Monika <monsulit@gmail.com>
2024-11-29 13:25:28 +0200

    Made strncmp not crash when fed NULL

--- libft/srcs/ft_strncmp.c
+++ libft/srcs/ft_strncmp.c
@@ -21,6 +21,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!s1 || !s2)
+		return (-1);
 	if (n == 0)
 		return (0);
 	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
