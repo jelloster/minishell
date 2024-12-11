253bf41e420703b207324fa6bd1e4d037b1eae15
Monika <monsulit@gmail.com>
2024-12-11 15:18:18 +0200

    Improved error messages

--- built_in_utils.c
+++ built_in_utils.c
@@ -53,13 +53,25 @@ static int	ft_isdigit_str(char *str)
 
 static void	exit_built_in(t_cmd *cmd, t_ms *ms)
 {
+	printf("exit\n");
 	if (!cmd->args[1])
 		exit (free_ms(ms, NULL, ms->cmds, ms->ret_val));
 	else
 	{
-		if (ft_isdigit_str(cmd->args[1]))
+		if (cmd->args[2])
+		{
+			//("%s: exit: too many arguments\n", ms->program_name);
+			write(2, ms->program_name + 2, ft_strlen(ms->program_name) - 2);
+			write(2, ": exit: too many arguments\n", 27); 
+			exit (free_ms(ms, NULL, ms->cmds, 2));
+		}
+		else if (ft_isdigit_str(cmd->args[1]))
 			exit (free_ms(ms, NULL, ms->cmds, ft_atoi(cmd->args[1])));
 		else
-			perror("Exit needs a numeric value\n");
+		{
+			write(2, ms->program_name + 2, ft_strlen(ms->program_name) - 2);
+			write(2, ": exit: numeric argument required\n", 34);
+			exit (free_ms(ms, NULL, ms->cmds, 2));
+		}
 	}
 }
