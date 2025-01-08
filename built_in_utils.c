983df762a90a769301c922b5ccaeb488f956a09d
Jimi Karhu <jkarhu@c1r5p6.hive.fi>
2025-01-08 14:47:04 +0200

    heredoc delete and cd~ not crashing

--- built_in_utils.c
+++ built_in_utils.c
@@ -6,7 +6,7 @@
 /*   By: motuomin <motuomin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 12:30:48 by motuomin          #+#    #+#             */
-/*   Updated: 2024/12/16 13:09:47 by motuomin         ###   ########.fr       */
+/*   Updated: 2025/01/08 14:40:15 by jkarhu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	is_built_in(const char *cmd)
 {
-	return (!ft_strncmp(cmd, "cd", 2)
+	return (!ft_strncmp(cmd, "cd", 3)
 		|| !ft_strncmp(cmd, "export", 7)
 		|| !ft_strncmp(cmd, "unset", 6)
 		|| !ft_strncmp(cmd, "echo", 5)
@@ -29,7 +29,7 @@ int	exe_built_in(t_cmd *cmd, t_ms *ms)
 	int	ret;
 
 	ret = 1;
-	if (!ft_strncmp(cmd->args[0], "cd", 2))
+	if (!ft_strncmp(cmd->args[0], "cd", 3))
 		return (cd_built_in(cmd->args, ms));
 	else if (!ft_strncmp(cmd->args[0], "export", 6))
 		return (export_built_in(cmd->args, ms, &ms->shell_vars));
