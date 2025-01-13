87b968495d59f6330b8b93152ae3caaa82244a2e
Jimi Karhu <jkarhu95@gmail.com>
2025-01-13 08:16:18 +0200

    moewsmallfix

--- main.c
+++ main.c
@@ -76,7 +76,7 @@ static int	exe_or_pipe(t_ms *ms, t_cmd *cmds)
 		ms->ret_val = exe_built_in(cmds, ms);
 	else
 		ms->ret_val = ms->temp_ret;
-	if (access(".heredoc_temp", R_OK == 0))
-		unlink(".heredoc_temp");
+	if (cmds->infile)
+		free(cmds->infile);
 	return (free_cmds(cmds, ms->parsed_cmds), 1);
 }
